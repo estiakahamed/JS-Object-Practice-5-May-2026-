84ca9d4c748744b0d7630dba2078c27c21f7b9cf
Estiak Ahamed <estiakahamed@gmail.com>
2026-05-26 07:27:42 +0600

    Add files via upload

---
+++ JS Object Practice (5 May 2026).js
@@ -0,0 +1,19 @@
+let FineCalCu={
+Roll: '31',
+Age: '30',
+Home: 'Meherpur',
+
+Office_Details:{
+    Address: 'SegunBagicha',
+    Time: '1-5',
+    Break: '1 Hours',
+
+    Office_Salary:{
+        Amount: '30k',
+        Date: ['End of maonth 30k', "middle of month 15k"]
+    }
+}
+}
+
+console.log(FineCalCu.Roll);
+console.log(FineCalCu.Office_Details.Office_Salary.Date[1]);
